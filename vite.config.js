bf56f2acdc7c6fbf1d8b15822ac0a0fb02882039
nitslaszlo <nitslaszlo@gmail.com>
2023-09-29 21:36:09 +0200

    upgrade vith debug

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,22 @@ import { defineConfig } from "vite";
 import { VitePluginNode } from "vite-plugin-node";
 
 export default defineConfig({
-  ssr: {
-    format: "cjs",
-  },
-  server: {
-    port: 8080,
-    open: true,
-    watch: true,
-  },
-  build: {
-    sourcemap: true,
-  },
-  plugins: [
-    ...VitePluginNode({
-      adapter: "express",
-      appPath: "./src/app.ts",
-      sourcemap: true,
-    }),
-  ],
+    ssr: {
+        format: "esm",
+    },
+    server: {
+        port: 8080,
+        open: true,
+        watch: true,
+    },
+    build: {
+        sourcemap: true,
+    },
+    plugins: [
+        ...VitePluginNode({
+            adapter: "express",
+            appPath: "./src/app.ts",
+            sourcemap: true,
+        }),
+    ],
 });
